716330e905ad4a1fb5b4c80a13992d375501d511
MattiaPT <mattia.taiana@gmail.com>
2023-10-15 16:02:31 +0200

    started on directory removal

--- cleaner.c
+++ cleaner.c
@@ -13,9 +13,11 @@
 
 #include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <unistd.h>
 
 #define DEFAULT_FOLDER "/Downloads"
 
@@ -32,24 +34,68 @@ int main(int argc, char *argv[]) {
 
   const int current_time = (int)time(NULL);
 
-  if (d) {
-    char base[256];
-    while ((dir = readdir(d)) != NULL) {
-      base[0] = '\0';
-      strcpy(base, folder_to_clean);
-      strcat(base, dir->d_name);
-
-      struct stat metadata;
-      stat(base, &metadata);
-
-      int timestamp = CRITERION(metadata);
-
-      if (timestamp + MAX_LIFETIME_S < current_time) {
-        printf("%d to delete: %s\n", timestamp, base);
-      }
-    }
-    closedir(d);
+  if (!d) {
+    printf("Failed to open directory at %s\n", folder_to_clean);
+    return -1;
   }
+
+  char base[256];
+  int counter = 0;
+  while ((dir = readdir(d)) != NULL) {
+    if (strcmp(dir->d_name, "..") == 0 || strcmp(dir->d_name, ".") == 0) {
+      continue;
+    }
+    base[0] = '\0';
+    strcpy(base, folder_to_clean);
+    strcat(base, dir->d_name);
+
+    struct stat metadata;
+    stat(base, &metadata);
+
+    int timestamp = CRITERION(metadata);
+
+    if (timestamp + MAX_LIFETIME_S < current_time) {
+      // printf("removing: %s\n", dir->d_name);
+      if (remove(base) != 0) {
+        strcat(base, "/");
+        printf("failed at: %s\n", base);
+        rmdir_recursively(base);
+      }
+      counter++;
+    }
+  }
+  closedir(d);
+  printf("Successfully removed %d elements from %s\n", counter,
+         folder_to_clean);
+
+  return 0;
+}
+
+int rmdir_recursively(const char *directory_location) {
+  DIR *d = opendir(directory_location);
+  struct dirent *dir;
+
+  if (!d) {
+    printf("Failed to open directory at %s\n", directory_location);
+    return -1;
+  }
+
+  char base[256];
+  while ((dir = readdir(d)) != NULL) {
+    if (strcmp(dir->d_name, "..") == 0 || strcmp(dir->d_name, ".") == 0) {
+      continue;
+    }
+    base[0] = '\0';
+    strcpy(base, directory_location);
+    strcat(base, dir->d_name);
+
+    if (remove(base) != 0) {
+      strcat(base, "/");
+      rmdir_recursively(base);
+    }
+  }
+
+  rmdir(directory_location);
 
   return 0;
 }
--- cleaner.h
+++ cleaner.h
@@ -1,6 +1,6 @@
 #ifndef CLEANER_H_
 #define CLEANER_H_
 
-
+int rmdir_recursively(const char *);
 
 #endif // CLEANER_H_
